3e1e3c8c45ed65428a9587c1b3094688989abd48
Bruno V. Mondragon <mondragon.si@gmail.com>
2022-09-11 09:02:54 -0400

    completed example

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
-from turtle import color
 import pandas as pd
 import matplotlib.pyplot as plt
-from turtle import *
-from tkinter import *
+import numpy as np
+import sklearn.linear_model
 
 money_data_frame = pd.read_csv("money.csv") 
 money_data_frame.head()
@@ -20,5 +19,21 @@ money_happiness_df.head(10)
 
 money_happiness_df['GDP'] = money_happiness_df['GDP'].apply(float)
 
-money_happiness_df.plot(kind='scatter', x='GDP', y='Happiness',color='red')
+money_happiness_df.plot(kind='scatter', x='GDP', y='Happiness',color='blue')
+#
+# plt.show()
+
+X = np.array(money_happiness_df['GDP']).reshape(-1,1)
+y = money_happiness_df['Happiness']
+
+linear_regression_model = sklearn.linear_model.LinearRegression()
+linear_regression_model.fit(X, y)
+
+prediction_y = linear_regression_model.predict(X)
+
+plt.scatter(X, y,  color='blue')
+plt.plot(X,prediction_y, color='red')
 plt.show()
+
+argentina_gdp = [[13588.84]]
+print(linear_regression_model.predict(argentina_gdp))
--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
+from turtle import color
 import matplotlib.pyplot as plt
-plt.plot([1, 2, 3, 4])
+plt.plot([1, 2, 3, 4],color='red')
 plt.ylabel('some numbers')
 plt.show()
